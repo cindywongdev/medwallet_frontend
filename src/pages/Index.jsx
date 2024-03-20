d5b6ec36985712760d344a735f897d9118f9e26a
Cindy Wong <cindywongdev@gmail.com>
2024-03-20 16:49:43 -0400

    pagination completed, starting to separate API calls

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,17 +1,35 @@
+import { useState, useEffect } from "react"
+import { useLoaderData } from "react-router-dom"
+import ReactPaginate from "react-paginate"
 import Record from "../components/Record"
 import FieldName from "../components/FieldName"
-import { useLoaderData } from "react-router-dom"
 
 const Index = (props) => {
-    const records = useLoaderData()
-    console.log(records.length)
+    let data = useLoaderData()
+    console.log(data.length)
 
     const fieldNames = ["Record ID", "Recipient Type", "Full Name", "State", "City", "Paying Entity", "Amount ($)", "Date", "Nature of Payment"]
+
+    const [currentPage, setCurrentPage] = useState(1)
+    const recordsPerPage = 10
+    const lastIndex = currentPage * recordsPerPage
+    const firstIndex = lastIndex - recordsPerPage
+    const records = data.slice(firstIndex, lastIndex)
+    const numPages = Math.ceil(data.length / recordsPerPage)
+    console.log(records.length)
+    console.log(numPages)
+
+
+    const handlePageClick = (e) => {
+        const page_number = e.selected + 1
+        setCurrentPage(page_number)
+    }
 
     return <div className="index-container" class="
                 border-2
                 border-black
                 w-full
+                min-h-screen
                 mt-8
                 rounded-2xl
             ">
@@ -39,6 +57,26 @@ const Index = (props) => {
             <Record record={record} key={record.record_id}/>
         )}
             </div>
+
+            <ReactPaginate
+                previousLabel={"Prev"}
+                nextLabel={"Next"}
+                breakLabel={"..."}
+                pageCount={numPages}
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={3}
+                onPageChange={handlePageClick}
+                containerClassName={"pagination justify-content-center"}
+                pageClassName={"page-item"}
+                pageLinkClassName={"page-link"}
+                previousClassName={"page-item"}
+                previousLinkClassName={"page-link"}
+                nextClassName={"page-item"}
+                nextLinkClassName={"page-link"}
+                breakClassName={"page-item"}
+                breakLinkClassName={"page-link"}
+                activeClassName={"active"}
+            />
 
     </div>
     {/* <h1>{records.one}</h1>
