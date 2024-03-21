3f2b622bd150284e5b6b7f8fa3ecba1c87ffc364
Cindy Wong <cindywongdev@gmail.com>
2024-03-21 19:02:11 -0400

    styled button to export to xls

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -1,6 +1,7 @@
 // import { useEffect } from "react"
 import { useOutletContext, useNavigate } from "react-router-dom"
 import RecordsDisplay from "../components/RecordsDisplay"
+import { TbDownload } from "react-icons/tb";
 
 const SearchResults = () => {
     // const navigate = useNavigate()
@@ -13,6 +14,16 @@ const SearchResults = () => {
     // }
 
     return <div>
+        <div className="flex justify-between items-center mt-4">
+            <p className="text-lg px-1">Search Results</p>
+
+            
+            <button className="hover:bg-blue-600 hover:text-white transition-colors">
+                <div className="flex items-center gap-2">
+                    Export to Excel <TbDownload size={20}/>
+                </div>
+            </button>
+        </div>
         <RecordsDisplay data={searchResults}/>
     </div>
 }
