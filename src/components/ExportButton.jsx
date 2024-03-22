7c3a30d8c5f493686a8cc85eeedf64702ddd74e0
Cindy Wong <cindywongdev@gmail.com>
2024-03-21 21:24:39 -0400

    completed export to excel functionality

--- src/components/ExportButton.jsx
+++ src/components/ExportButton.jsx
@@ -1,7 +1,7 @@
 import { TbDownload } from "react-icons/tb";
 
-const ExportButton = (props) => {
-    return <button className="hover:bg-blue-600 hover:text-white transition-colors">
+const ExportButton = ({exportToExcel}) => {
+    return <button onClick={exportToExcel} className="hover:bg-blue-600 hover:text-white transition-colors">
         <div className="flex items-center gap-2">
             Export to Excel <TbDownload size={20} />
         </div>
