f2ee8e3692df5eed845ca74779703793097c0c65
Cindy Wong <cindywongdev@gmail.com>
2024-03-22 01:48:37 -0400

    moved export to excel logic to export button

--- src/components/ExportButton.jsx
+++ src/components/ExportButton.jsx
@@ -1,6 +1,29 @@
 import { TbDownload } from "react-icons/tb";
+import { utils, write } from "xlsx";
+import { saveAs } from "file-saver";
 
-const ExportButton = ({exportToExcel}) => {
+const ExportButton = ({searchResults}) => {
+    const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+    const EXCEL_EXTENSION = '.xlsx';
+
+    const exportToExcel = () => {
+        const worksheet = utils.json_to_sheet(searchResults)
+        const workbook = {
+            Sheets: {
+                'data': worksheet
+            },
+            SheetNames: ['data']
+        }
+
+        const excelBuffer = write(workbook, {bookType: 'xlsx', type: 'array'})
+        saveAsExcel(excelBuffer, 'searchResults')
+    }
+
+    const saveAsExcel = (buffer, filename) => {
+        const data = new Blob([buffer], { type: EXCEL_TYPE })
+        saveAs(data, filename + '_export' + new Date().getTime() + EXCEL_EXTENSION)
+    }
+
     return <button data-test="export-button" onClick={exportToExcel} className="hover:bg-blue-600 hover:border-blue-600 hover:text-white transition-colors">
         <div className="flex items-center gap-2">
             Export to Excel <TbDownload size={20} />
--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -2,8 +2,6 @@
 import { useOutletContext, useNavigate } from "react-router-dom"
 import RecordsDisplay from "../components/RecordsDisplay"
 import ExportButton from "../components/ExportButton";
-import { utils, write } from "xlsx";
-import { saveAs } from "file-saver";
 
 const SearchResults = () => {
     // const navigate = useNavigate()
@@ -15,31 +13,10 @@ const SearchResults = () => {
     //         navigate("/")
     // }
 
-    const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
-    const EXCEL_EXTENSION = '.xlsx';
-
-    const exportToExcel = () => {
-        const worksheet = utils.json_to_sheet(searchResults)
-        const workbook = {
-            Sheets: {
-                'data': worksheet
-            },
-            SheetNames: ['data']
-        }
-
-        const excelBuffer = write(workbook, {bookType: 'xlsx', type: 'array'})
-        saveAsExcel(excelBuffer, 'searchResults')
-    }
-
-    const saveAsExcel = (buffer, filename) => {
-        const data = new Blob([buffer], { type: EXCEL_TYPE })
-        saveAs(data, filename + '_export' + new Date().getTime() + EXCEL_EXTENSION)
-    }
-
     return <div>
         <div className="flex justify-between items-end mt-4">
             <p className="text-lg px-1">Search Results</p>
-            <ExportButton exportToExcel={exportToExcel}/>
+            <ExportButton searchResults={searchResults}/>
         </div>
         <RecordsDisplay data={searchResults}/>
     </div>
