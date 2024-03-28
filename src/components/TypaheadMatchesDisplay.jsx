4abe1b98b4a14b2209c74e8c12b57ecb995c77cd
Cindy Wong <cindywongdev@gmail.com>
2024-03-28 01:06:30 -0400

    converted if else to ternary

--- src/components/TypaheadMatchesDisplay.jsx
+++ src/components/TypaheadMatchesDisplay.jsx
@@ -1,21 +1,17 @@
 import TypaheadMatch from "./TypaheadMatch"
 
 const TypaheadMatchesDisplay = ({ typaheadMatches, handleMatchClick }) => {
-    {
-        if (typaheadMatches.length > 0) {
-            return <ul className="hidden peer-focus:block hover:block border-2 border-gray-500 rounded-b-lg w-72 absolute top-10 p-2 bg-white">
-                {typaheadMatches.map((match) => {
-                    return <TypaheadMatch text={match} key={match} handleMatchClick={handleMatchClick}/>
-                })}
+    return typaheadMatches.length > 0 ?
+        <ul className="hidden peer-focus:block hover:block border-2 border-gray-500 rounded-b-lg w-72 absolute top-10 p-2 bg-white">
+            {typaheadMatches.map((match) => {
+                return <TypaheadMatch text={match} key={match} handleMatchClick={handleMatchClick} />
+            })}
 
-            </ul>
-        } else {
-            return <ul className="hidden peer-focus:block hover:block border-2 border-gray-500 rounded-b-lg w-72 absolute top-10 p-2 bg-white">
-                <p className="p-1">No results found.</p>
-            </ul>
-        }
-    }
-
+        </ul>
+        :
+        <ul className="hidden peer-focus:block hover:block border-2 border-gray-500 rounded-b-lg w-72 absolute top-10 p-2 bg-white">
+            <p className="p-1">No results found.</p>
+        </ul>
 }
 
 export default TypaheadMatchesDisplay
