e219ac05608790d25010c8882689209bb1374d97
Cindy Wong <cindywongdev@gmail.com>
2024-03-27 22:32:30 -0400

    wrote checkTypaheadMatches function to check if search q is a substr of valid queries

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,9 +2,25 @@ import { FaMagnifyingGlass } from "react-icons/fa6";
 import TypaheadMatchesDisplay from "./TypaheadMatchesDisplay";
 
 const SearchBar = ({onRecipTypeQChange, handleSearch, recipTypeQ, validRecipTypes, typaheadMatches, setTypaheadMatches}) => {
-    console.log(validRecipTypes)
+    // console.log(validRecipTypes)
 
-    return <div className="flex items-center gap-6 px-4">
+    const checkTypaheadMatches = () => {
+        let matches = []
+
+        for (const recipient_type of validRecipTypes) {
+            if (recipient_type.includes(recipTypeQ.toLowerCase())) {
+                matches.push(recipient_type)
+            }
+        }
+
+        console.log(matches)
+
+        // setTypaheadMatches(matches)
+    }
+
+    checkTypaheadMatches()
+
+    return <div className="flex items-center gap-6 px-4 border-2 border-green-500 relative">
         <FaMagnifyingGlass size={25}/>
         <label htmlFor="search_form" className="text-lg font-medium">Search:</label>
         <form data-test="search-form" onSubmit={handleSearch} method="GET" id="search_form" className="flex gap-4">
@@ -15,11 +31,12 @@ const SearchBar = ({onRecipTypeQChange, handleSearch, recipTypeQ, validRecipType
                 rounded-lg
                 p-2
                 w-72
+                z-10
             "/>
+            <TypaheadMatchesDisplay typaheadMatches={typaheadMatches}/>
 
             <button data-test="submit-button" type="submit" form="search_form" value="Submit" className="hover:text-white hover:bg-gray-600 hover:border-gray-600 transition-colors">Submit</button>
         </form>
-        <TypaheadMatchesDisplay typaheadMatches={typaheadMatches}/>
     </div>
 }
 
--- src/components/TypaheadMatchesDisplay.jsx
+++ src/components/TypaheadMatchesDisplay.jsx
@@ -1,9 +1,11 @@
 const TypaheadMatchesDisplay = ({typaheadMatches}) => {
     // console.log("we in here: ", typaheadMatches)
 
-    return <ul className="border-2 border-red-500">
+    return <ul className="border-2 border-red-500 absolute top-10 p-2 bg-blue-500">
         {typaheadMatches.map((match) => {
-            return <p>{match}</p>
+            return <p key={match} onClick={() => {
+                //
+            }} className="p-1 hover:text-red-500 hover:cursor-pointer">{match}</p>
         })}
     </ul>
 }
